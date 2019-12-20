9c2d4ef353a7cf9149e7c99e84dd67c86f1cd28a
Ben F <56423163+bferriman@users.noreply.github.com>
2019-12-20 18:00:39 -0500

    wrote code to get access token and do a pet search

--- assets/javascript/petfinder-api.js
+++ assets/javascript/petfinder-api.js
@@ -1,9 +1,39 @@
+var PetfinderAPIKey = "WdIiOOJyE03Hnp7raZfKwVZbInn5Nb7ZIQISdDaCo3aclUJHDj";
+var PetfinderAPISecret = "SgOkPoh9MPc11jYtWQp3ukO1tcpFH0fAz5U3UbBn";
+
+
 //called once we have enough data; determine search parameters and make API call
 function searchForCats(){
+    var queryURL = "https://api.petfinder.com/v2/oauth2/token";
+    var queryData = "grant_type=client_credentials&client_id=" + PetfinderAPIKey + "&client_secret=" + PetfinderAPISecret;
 
+    $.ajax({
+        url: queryURL,
+        method: "POST",
+        data: queryData,
+    }).then(function(response){
+        var token = response.access_token;
+
+        var requestHeader = "Bearer " + token;
+        queryURL = "https://api.petfinder.com/v2/animals";
+        var queryParameters = "?type=cat&breed=siamese";
+
+        $.ajax({
+            url: queryURL + queryParameters,
+            method: "GET",
+            headers: {
+                "Authorization" : requestHeader
+            }
+        }).then(function(response){
+            console.log(response);
+            console.log(response.animals[0].url);
+        });
+    });
 }
 
 //build and return a jQuery object to match the .pet-result divs in cat-select.html
 function buildPetResultDiv(){
 
 }
+
+searchForCats();   //this is just a placeholder call for testing
