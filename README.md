31410aa413c2d4ff342a46fde8dfebcbd2c2490f
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2019-12-19 20:04:50 -0500

    added images & updated README and cat-image-library.js

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# pet-spiration
-find and display info about shelter pets
+# PetSpiration
+
+## Description
+
+Find and display info about shelter pets
+
+## Credits
+
+- "short.black.adult.jpg" Photo by Nathan Riley on Unsplash
+- "short.orange.kitten.jpg" Photo by Matthew Larkin on Unsplash
---
+++ assets/javascript/cat-image-library.js
@@ -0,0 +1,15 @@
+// Create constant to store cat library objects
+const catLibrary = [
+  {
+    age: "Adult",
+    color: "Black",
+    coat: "Short Hair",
+    image: "../images/short.black.adult.jpg"
+  },
+  {
+    age: "Kitten",
+    color: "Orange",
+    coat: "Short Hair",
+    image: "../images/short.orange.kitten.jpg"
+  }
+];
