ffe193effbb7ba7eb95ce757f7855d80b89a79cb
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2019-12-21 10:38:34 -0500

    pseudocoded start button click event

--- assets/javascript/show-cats-button.js
+++ assets/javascript/show-cats-button.js
@@ -1,5 +1,7 @@
 //need to get a first photo and do the DOM work
 
-$("#startButton").on("click", function(){
-
-})
+$("#startButton").on("click", function() {
+  // Hide the about section and adjust height of main content section
+  // Populate cat photo
+  // Populate like and dislike buttons
+});
