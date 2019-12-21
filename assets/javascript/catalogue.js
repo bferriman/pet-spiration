e9919a3cd6f884b3fa71694abf1b9b0c3a733834
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2019-12-21 11:40:38 -0500

    created displayPhoto function

--- assets/javascript/catalogue.js
+++ assets/javascript/catalogue.js
@@ -1,45 +1,35 @@
 //add the attribute count variables here
 
-
-
 //event listener for thumb up
-$(document).on("click", "#likeButton", function(event){
-    //capture attributes of current image and store in variables
-
-    //increment appropriate global attribute count tracking variables
-
-    //check whether we have sufficient data to proceed to cat select page
-
-    //if not, select a photo to show next
-
-    //update DOM with new photo
+$(document).on("click", "#likeButton", function(event) {
+  //capture attributes of current image and store in variables
+  //increment appropriate global attribute count tracking variables
+  //check whether we have sufficient data to proceed to cat select page
+  //if not, select a photo to show next
+  //update DOM with new photo
 });
 
 //event listener for thumb down
-$(document).on("click", "#dislikeButton", function(event){
-    //capture attributes of current image and store in variables
-
-    //increment appropriate global attribute count tracking variables
-
-    //check whether we have sufficient data to proceed to cat select page
-
-    //if not, select a photo to show next
-
-    //update DOM with new photo
+$(document).on("click", "#dislikeButton", function(event) {
+  //capture attributes of current image and store in variables
+  //increment appropriate global attribute count tracking variables
+  //check whether we have sufficient data to proceed to cat select page
+  //if not, select a photo to show next
+  //update DOM with new photo
 });
 
 //evaluates the data we've gathered so far and returns true if we have enough data to move on to cat select, false if not
-function haveEnoughData(){
-
-}
+function haveEnoughData() {}
 
 //build an algorithm to select a next photo based on what attributes we need more data for.
 //returns the next photo object to be displayed
-function getNextPhoto(){
-
-}
+function getNextPhoto() {}
 
 //updates DOM, replacing old photo with new photo passed to the function
-function displayPhoto(photo){
-
+function displayPhoto(photo) {
+  // Clear the main content div
+  $("#main-content-div").empty();
+  // Append a div with id="catPhoto"
+  $("#main-content-div").append($('<div id="catPhoto"></div>'));
+  $("#catPhoto").css("background-image", "url(" + photo + ")");
 }
--- assets/javascript/show-cats-button.js
+++ assets/javascript/show-cats-button.js
@@ -1,14 +1,23 @@
 // declare variables
 const aboutSection = $("#about-section");
 const mainContentHeight = $(".main-content-height");
+let randomNum;
+let randomStockCatAge;
+let randomStockCatCoat;
+let randomStockCatColor;
+let randomStockCatBreed;
+let randomStockCatImage;
 
-//need to get a first photo and do the DOM work
-
+// When user clicks the "Show me cats!" button...
 $("#startButton").on("click", function() {
-  // Hide the button & the about section, and adjust height of main content section
+  // Hide the about section
   aboutSection.hide();
-  $("#startButton").hide();
+  //   $("#startButton").hide();
+  // Adjust height of main content section
   mainContentHeight.css("min-height", "calc(100vh - 58px - 4px - 58px)");
-  // Populate cat photo
+  // Populate random cat photo from cat-image-library.js
+  randomNum = Math.floor(Math.random() * catLibrary.length);
+  randomStockCatImage = catLibrary[randomNum].image;
+  displayPhoto(randomStockCatImage);
   // Populate like and dislike buttons
 });
