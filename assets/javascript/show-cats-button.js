1e94aa6368f67b61c8500d3d756d9a1bbcdb1a05
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2019-12-21 10:52:03 -0500

    add event to hide about section & start button, change main content height

--- assets/javascript/show-cats-button.js
+++ assets/javascript/show-cats-button.js
@@ -1,7 +1,14 @@
+// declare variables
+const aboutSection = $("#about-section");
+const mainContentHeight = $(".main-content-height");
+
 //need to get a first photo and do the DOM work
 
 $("#startButton").on("click", function() {
-  // Hide the about section and adjust height of main content section
+  // Hide the button & the about section, and adjust height of main content section
+  aboutSection.hide();
+  $("#startButton").hide();
+  mainContentHeight.css("min-height", "calc(100vh - 58px - 4px - 58px)");
   // Populate cat photo
   // Populate like and dislike buttons
 });
